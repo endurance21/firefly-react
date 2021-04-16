406ce77c057bd4370a838098baba69c8a59253e5
Divyanshu Raj <divyanshuraj5050@gmail.com>
2021-04-17 04:31:35 +0530

    fixed dependecies version conflict

--- dist/components/canvas.d.ts
+++
@@ -1,3 +0,0 @@
-/// <reference types="react" />
-import "../styles.scss";
-export default function Canvas(): JSX.Element;
--- dist/index.d.ts
+++
@@ -1,3 +0,0 @@
-import React from "react";
-declare const ReactFirefly: React.FC<any>;
-export default ReactFirefly;
