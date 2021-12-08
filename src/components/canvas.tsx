6557d49d28089f668edf4e8e3cbf3a1e1b16ef77
DIVYANSHU RAJ <43696525+endurance21@users.noreply.github.com>
2021-12-08 23:25:07 +0530

    added documentation

--- src/components/canvas.tsx
+++ src/components/canvas.tsx
@@ -1,8 +1,15 @@
 import React,{MutableRefObject} from "react"
+
 type props = {
   canvasRef :any
   className:string
 }
+
+        
+/**
+ * React component wrapper for actual html5Canvas element.
+ * @constructor
+ */
 export default function Canvas(props:props):JSX.Element{
   return (<canvas ref={props.canvasRef} className={props.className}></canvas>)
 }
