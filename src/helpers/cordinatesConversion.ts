24938d6364105412aaef134635bb10737a2986c3
Divyanshu Raj <divyanshuraj5050@gmail.com>
2021-12-09 02:02:30 +0530

    added prettier and formated the codebase

--- src/helpers/cordinatesConversion.ts
+++ src/helpers/cordinatesConversion.ts
@@ -5,37 +5,34 @@
  * @returns {Object} cartesian cordinate.
  */
 
-export function polarToCartesian(r:number, theta:number){
-
-    let x = r*Math.cos(theta*Math.PI/180);
-    let y = r*Math.sin(theta*Math.PI/180);
-    return {x,y}
+export function polarToCartesian(r: number, theta: number) {
+  let x = r * Math.cos((theta * Math.PI) / 180);
+  let y = r * Math.sin((theta * Math.PI) / 180);
+  return { x, y };
 }
 /**
- * convert cordinate from cartesian to polar .  
+ * convert cordinate from cartesian to polar .
  * @param {Number} x - x-cordinate of the pont.
  * @param {Number} y - y-cordinate of the point.
  * @returns {Object} cartesian cordinate.
  */
 
-export function cartesianToPolar(x:number,y:number){
-
-    let r = Math.sqrt(x*x + y*y)
-    let theta = Math.atan2(y,x) // in degree
-    theta = normalizeAngle(theta)
-    return {r,theta}
+export function cartesianToPolar(x: number, y: number) {
+  let r = Math.sqrt(x * x + y * y);
+  let theta = Math.atan2(y, x); // in degree
+  theta = normalizeAngle(theta);
+  return { r, theta };
 }
 
 /**
- *  normalizes the given angle 
+ *  normalizes the given angle
  * @param {Number} theta - angle with x axis.
  * @returns {Number} the same angle if its positive and normalized if it is negatives
  */
 
-export function normalizeAngle(theta){
- return radianToDegree((theta >= 0 ? theta : (2*Math.PI + theta)))
+export function normalizeAngle(theta) {
+  return radianToDegree(theta >= 0 ? theta : 2 * Math.PI + theta);
 }
-
 
 /**
  * Perfoms addition of two vector that are in polar forms.
@@ -43,37 +40,44 @@ export function normalizeAngle(theta){
  * @param {Object}     vector2 - second vector.
  * @returns {Object} new vector in polar form.
  */
-export function vectorSumPolar(vector1,vector2){
-    let {r:r1,theta:theta1} = vector1
-    let {r:r2,theta:theta2} = vector2
-    let r = Math.sqrt(r1*r1 + r2*r2 + 2*r1*r2*Math.cos(degreeToRadian(theta1 - theta2)))
-    let theta =  inBound(theta1 + normalizeAngle(Math.atan2(r2*Math.sin(degreeToRadian(theta2-theta1)), r1 +  r2*Math.cos(degreeToRadian(theta2-theta1)))))
+export function vectorSumPolar(vector1, vector2) {
+  let { r: r1, theta: theta1 } = vector1;
+  let { r: r2, theta: theta2 } = vector2;
+  let r = Math.sqrt(r1 * r1 + r2 * r2 + 2 * r1 * r2 * Math.cos(degreeToRadian(theta1 - theta2)));
+  let theta = inBound(
+    theta1 +
+      normalizeAngle(
+        Math.atan2(
+          r2 * Math.sin(degreeToRadian(theta2 - theta1)),
+          r1 + r2 * Math.cos(degreeToRadian(theta2 - theta1)),
+        ),
+      ),
+  );
 
-    return {r,theta}
-
+  return { r, theta };
 }
 
 /**
- * converts degree to radian. 
+ * converts degree to radian.
  * @param {Number} degree - angle given in degree
  * @returns {Number} angle in Radian.
  */
-export function degreeToRadian(degree){
-    return degree*Math.PI/180
+export function degreeToRadian(degree) {
+  return (degree * Math.PI) / 180;
 }
 /**
- * converts radian to degree  
+ * converts radian to degree
  * @param {Number} radian - angle given in radian.
  * @returns {Number} angle in degree.
  */
-export function radianToDegree(radian){
-    return inBound((radian*180/Math.PI))
+export function radianToDegree(radian) {
+  return inBound((radian * 180) / Math.PI);
 }
 /**
  * checks if angle becomes greater than 360 bound.
  * @param {Number} degree - angle given in degree
  * @returns {Number}    inbound angle
  */
-export function inBound(degree){
-    return (degree <=360 ? degree : degree % 360)
+export function inBound(degree) {
+  return degree <= 360 ? degree : degree % 360;
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,123 +1,136 @@
-import React,{useRef, useState, useEffect} from "react"
-import Canvas from "./components/canvas"
-import  {Firefly} from "./components/firefly"
-import {getRandomInt} from "./helpers/random"
-import {throttle} from "./helpers/throttle"
+import React, { useRef, useState, useEffect } from "react";
+import Canvas from "./components/canvas";
+import { Firefly } from "./components/firefly";
+import { getRandomInt } from "./helpers/random";
+import { throttle } from "./helpers/throttle";
 
-type props ={
-  canvasWidth:number
-  canvasHeight:number
-  colors:string[]
-  className:string
+type props = {
+  canvasWidth: number;
+  canvasHeight: number;
+  colors: string[];
+  className: string;
+};
 
-}
+const fireflies: Firefly[] = [];
 
-const fireflies  :Firefly[] = []
+const giveBirth: any = throttle(function (
+  mouse,
+  numberOfBirths,
+  colors,
+  changeDirectionFrequency,
+  randomMotion,
+) {
+  let { x, y } = mouse;
+  if (x === null || y === null) return;
+  for (let i = 0; i < numberOfBirths; i++) {
+    let velocity = { speed: getRandomInt(2, 3), direction: getRandomInt(0, 360) };
+    let color = colors[getRandomInt(0, colors.length)];
+    let directionTurner = { magnitude: getRandomInt(1, 4), direction: getRandomInt(5, 10) };
+    let firefly = new Firefly(
+      { x, y },
+      velocity,
+      directionTurner,
+      changeDirectionFrequency,
+      color,
+      null,
+      null,
+      randomMotion,
+    );
+    fireflies.push(firefly);
+  }
+},
+95);
 
-const giveBirth :any = throttle(function (mouse,numberOfBirths,colors,changeDirectionFrequency,randomMotion){
-  let {x,y} = mouse
-  if(x === null || y === null)
-  return
-  for(let i = 0;i<numberOfBirths;i++){
-    let velocity = {speed:getRandomInt(2,3),direction:getRandomInt(0,360)}
-    let color = colors[getRandomInt(0,colors.length)]
-    let directionTurner = {magnitude:getRandomInt(1,4),direction:getRandomInt(5,10)}
-    let firefly = new Firefly({x,y},velocity,directionTurner,changeDirectionFrequency,color,null,null,randomMotion)
-    fireflies.push(firefly)
+const setFromEvent = (e, setMousePosition, boundary) => {
+  setMousePosition({ x: e.clientX - boundary.current.x, y: e.clientY - boundary.current.y });
+};
+const ReactFirefly = (props: props) => {
+  let numberOfBirths = 2;
+  let { canvasWidth, canvasHeight, colors, className } = props;
+
+  let changeDirectionFrequency = 20;
+  let randomMotion = true;
+
+  if (!colors || colors.length == 0) {
+    colors = ["#f15bb5", "#f72585"];
   }
 
-
-} , 95)
-
-const setFromEvent = (e,setMousePosition,boundary) =>{
-  setMousePosition({ x: e.clientX - boundary.current.x, y: e.clientY - boundary.current.y });
-}
-const ReactFirefly = (props:props)=>{
-  let  numberOfBirths = 2 ;
-  let{ canvasWidth, canvasHeight, colors,className } = props
-
-   let changeDirectionFrequency = 20;
-   let randomMotion = true
-
-   if(!colors || colors.length == 0){
-    colors = [
-        "#f15bb5","#f72585"]
-    }
-
-
-
-  const canvas = useRef<HTMLCanvasElement>()
-  const context = useRef(null)
-  const requestRef = useRef(0)
-  const boundary = useRef({x:0,y:0,width:canvasWidth,height:canvasHeight})
+  const canvas = useRef<HTMLCanvasElement>();
+  const context = useRef(null);
+  const requestRef = useRef(0);
+  const boundary = useRef({ x: 0, y: 0, width: canvasWidth, height: canvasHeight });
   const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
 
-
   useEffect(() => {
-    init()
-    animate()
+    init();
+    animate();
 
     return () => {
       cancelAnimationFrame(requestRef.current);
-      canvas.current.removeEventListener("mousemove", (e)=> setFromEvent(e,setMousePosition,boundary));
-      canvas.current.addEventListener("mousemove",(e)=> setFromEvent(e,setMousePosition,boundary));
-      window.removeEventListener("scroll",setBoundary)
-
-    }
+      canvas.current.removeEventListener("mousemove", (e) =>
+        setFromEvent(e, setMousePosition, boundary),
+      );
+      canvas.current.addEventListener("mousemove", (e) =>
+        setFromEvent(e, setMousePosition, boundary),
+      );
+      window.removeEventListener("scroll", setBoundary);
+    };
   }, []);
 
   useEffect(() => {
-    resizeCanvas()
-    setBoundary()
+    resizeCanvas();
+    setBoundary();
   }, [props]);
 
   useEffect(() => {
-   giveBirth(mousePosition,numberOfBirths, colors,changeDirectionFrequency,randomMotion)
- }, [mousePosition]);
+    giveBirth(mousePosition, numberOfBirths, colors, changeDirectionFrequency, randomMotion);
+  }, [mousePosition]);
 
+  const init = () => {
+    resizeCanvas();
+    context.current = canvas.current.getContext("2d");
 
+    setBoundary();
 
-  const init = ()=>{
-    resizeCanvas()
-    context.current = canvas.current.getContext("2d")
+    giveBirth(
+      { x: canvasWidth / 2, y: canvasHeight / 2 - 25 },
+      25,
+      colors,
+      changeDirectionFrequency,
+      randomMotion,
+    );
 
-    setBoundary()
-
-    giveBirth({x:canvasWidth/2, y:canvasHeight/2 -25},25,colors,changeDirectionFrequency,randomMotion)
-
-    canvas.current.addEventListener("mousemove",(e)=> setFromEvent(e,setMousePosition,boundary));
-    window.addEventListener("scroll",setBoundary)
-
-
-  }
-  const resizeCanvas = ()=>{
-    canvas.current.width = canvasWidth
-    canvas.current.height = canvasHeight
-  }
-  const setBoundary = ()=>{
+    canvas.current.addEventListener("mousemove", (e) =>
+      setFromEvent(e, setMousePosition, boundary),
+    );
+    window.addEventListener("scroll", setBoundary);
+  };
+  const resizeCanvas = () => {
+    canvas.current.width = canvasWidth;
+    canvas.current.height = canvasHeight;
+  };
+  const setBoundary = () => {
     let rect = canvas.current.getBoundingClientRect();
     let x = rect.left;
-    let y = rect.top
+    let y = rect.top;
     boundary.current.x = x;
     boundary.current.y = y;
     boundary.current.width = canvasWidth;
-    boundary.current.height = canvasHeight
-  }
-  const animate = ()=>{
+    boundary.current.height = canvasHeight;
+  };
+  const animate = () => {
     requestRef.current = requestAnimationFrame(animate);
-    context.current.clearRect(0, 0, canvasWidth, canvasHeight)
-    for(let i  = 0 ; i <  fireflies.length ; i++){
-      fireflies[i].update(boundary.current)
-      fireflies[i].draw(context.current)
+    context.current.clearRect(0, 0, canvasWidth, canvasHeight);
+    for (let i = 0; i < fireflies.length; i++) {
+      fireflies[i].update(boundary.current);
+      fireflies[i].draw(context.current);
 
-        if(fireflies[i].globalAlpha < 0.01 ){
-          fireflies.splice(i,1) //remove the dead firefly from array
-        }
-
+      if (fireflies[i].globalAlpha < 0.01) {
+        fireflies.splice(i, 1); //remove the dead firefly from array
+      }
     }
+  };
 
-  }
-
-return (<Canvas canvasRef={canvas} className={className} ></Canvas>)
-}
+  return <Canvas canvasRef={canvas} className={className}></Canvas>;
+};
 export default ReactFirefly;
