e09f4e84306108b7c2b73196d0bf17f3b97221b4
Divyanshu Raj <divyanshuraj5050@gmail.com>
2021-12-09 00:02:00 +0530

    added more documentations

--- src/helpers/random.ts
+++ src/helpers/random.ts
@@ -1,3 +1,9 @@
+/**
+ * genrates  a random number between given range
+ * @param {Number} min - lower bound
+ *  @param {Number} max - upper bound
+ * @returns {Number} a random generated integer
+ */
 export function getRandomInt(min,max){
     if(min === null && max)
     return max
